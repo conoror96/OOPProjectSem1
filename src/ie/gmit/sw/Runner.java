91f2980548987f4b6adcaa8db4e39ef65ebc81a7
kodama96 <g00338592@gmit.ie>
2018-01-09 16:59:23 +0000

    add header, scanner and bufferedReader to runner

--- src/ie/gmit/sw/Runner.java
+++ src/ie/gmit/sw/Runner.java
@@ -1,9 +1,24 @@
 package ie.gmit.sw;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.util.Scanner;
+
 public class Runner {
-	public static void main(String[] args)
-	 {
-		
-		
-	 }
+	public static void main(String[] args) {
+		// Project Header
+		System.out.println("---------------------------------------------------------");
+		System.out.println("     A Java API for Measuring Document Similarity");
+		System.out.println("G00338592 - Conor O'Reilly - Software Development Year 3");
+		System.out.println("----------------------------------------------------------");
+
+		// Scanner and BufferedReader to read file names
+		FileInputStream fileInputStream = null;
+		Scanner scanner = new Scanner(System.in);
+		BufferedReader bufferedReader = null;
+
+		// Variables
+
+		// Menu
+	}
 }
