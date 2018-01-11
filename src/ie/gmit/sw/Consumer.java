69b836c72108c45344e073ae4a26ae716b926f7b
kodama96 <g00338592@gmit.ie>
2018-01-11 17:25:20 +0000

    consumer class changes

--- src/ie/gmit/sw/Consumer.java
+++ src/ie/gmit/sw/Consumer.java
@@ -13,72 +13,67 @@ public class Consumer implements Runnable {
 	private BlockingQueue<Shingle> queue;
 	private int k;
 	private int[] minhashes; // The random stuff
-	private Map<Integer, List<Integer>> map = new HashMap<>();
-	private ExecutorService pool;
+	private Map<Integer, List<Integer>> map; // = new HashMap<>()
+	// private ExecutorService pool;
 
-	public Consumer(BlockingQueue<Shingle> q, int k, int poolSize) {
+	public Consumer(BlockingQueue<Shingle> q, int k, int[] hashes) {
 		this.queue = q;
 		this.k = k;
-		pool = Executors.newFixedThreadPool(poolSize);
+		this.map = map;
+		this.minhashes = hashes;
+		// pool = Executors.newFixedThreadPool(poolSize);
 		// ThreadPoolExecutor executor = (ThreadPoolExecutor)
 		// Executors.newFixedThreadPool(4); ---
 		// https://howtodoinjava.com/core-java/multi-threading/java-fixed-size-thread-pool-executor-example/
-		init();
+		// init();
 	}
 
-	private void init() {
-		// TODO Auto-generated method stub
-		Random random = new Random();
-		minhashes = new int[k]; // k = 200 - 300
-		for (int i = 0; i < minhashes.length; i++) {
-			minhashes[i] = random.nextInt(0);
-		}
-	}
+	/*
+	 * private void init() { // TODO Auto-generated method stub Random random =
+	 * new Random(); minhashes = new int[k]; // k = 200 - 300 for (int i = 0; i
+	 * < minhashes.length; i++) { minhashes[i] = random.nextInt(0); } }
+	 */
 
 	@Override
 	public void run() {
-		try {
-			int docCount = 2;
-			while (docCount > 0) {
+		// TODO Auto-generated method stub
+		int docCount = 1;
+		int max = Integer.MAX_VALUE;
+		int value = 0;
+
+		while (docCount > 0) {
+			try {
 				Shingle s = queue.take();
-				if (s instanceof Poison) {
+
+				if (s.getShingleHashCode() == 0) {
 					docCount--;
-				} else {
-					pool.execute(new Runnable() {
-						@Override
-						public void run() {
-							for (int i = 0; i < minhashes.length; i++) {
-								int value = s.getShingleHashCode() ^ minhashes[i]; // ^
-																					// -
-																					// xor(Random
-																					// generated
-																					// key)
-								List<Integer> list = map.get(s.getDocumentId());
-								if (list == null) {
-									list = new ArrayList<Integer>(k);
-									for (int j = 0; j < minhashes.length; j++) {
-										list.add(Integer.MAX_VALUE);
-										System.out.println(s.getDocumentId() + "  -----   " + value);
-									}
-									map.put(s.getDocumentId(), list);
+					continue;
+				}
 
-								} else {
-									if (list.get(i) > value) {
-										list.set(i, value);
+				List<Integer> list = map.get(s.getDocumentId());
 
-									}//if
-								}//else
-							}//for
-						}//run
-					});
+				if (list == null) {
+					list = new ArrayList<Integer>(k);
 
-				}//else
+					for (int j = 0; j < k; j++) {
+						list.add(j, max);
+					}
+					map.put(s.getDocumentId(), list);
+				}
+				for (int i = 0; i < minhashes.length; i++) {
+					value = s.getShingleHashCode() ^ minhashes[i];
+
+					if (list.get(i) > value) {
+						list.set(i, value);
+					}
+				}
 			}
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
-	}
+			catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 
-}
+			}
+		} // while
+	} // run method
+} //consumer class
